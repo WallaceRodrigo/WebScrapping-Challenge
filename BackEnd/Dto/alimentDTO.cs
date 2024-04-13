5a1b2063933fb18d5648df4bf5336dd49d1c14b7
Wallace Rodrigo <wallacerodrigo2004@gmail.com>
2024-04-13 14:13:57 -0300

    scrap id, name, scientificname, group and brand

---
+++ BackEnd/Dto/alimentDTO.cs
@@ -0,0 +1,11 @@
+namespace BackEnd.Dto
+{
+    public class alimentDTO
+    {
+        public string? id { get; set; }
+        public string? name { get; set; }
+        public string? scientificName { get; set; }
+        public string? group { get; set; }
+        public string? brand { get; set; }
+    }
+}
--- BackEnd/Services/WebScrap.cs
+++ BackEnd/Services/WebScrap.cs
@@ -1,10 +1,11 @@
 using HtmlAgilityPack;
+using BackEnd.Dto;
 
 namespace BackEnd.Services;
 
 public class WebScrap
 {
-	public string scrap()
+	public List<alimentDTO> scrap()
 	{
 		var html = @"https://www.tbca.net.br/base-dados/composicao_estatistica.php?pagina=1&atuald=1#";
 
@@ -12,8 +13,21 @@ public class WebScrap
 
         var htmlDoc = web.Load(html);
 
-        var node = htmlDoc.DocumentNode.SelectSingleNode("//head/title");
+        var node = htmlDoc.DocumentNode.SelectNodes("//tr");
 
-        return("Node Name: " + node.Name + "\n" + node.OuterHtml);		
+        return node.Select(node => {
+
+          var Aliments = new alimentDTO
+          {
+            id = node.ChildNodes[0].InnerText,
+            name = node.ChildNodes[1].InnerText,
+            scientificName = node.ChildNodes[2].InnerText,
+            group = node.ChildNodes[3].InnerText,
+            brand = node.ChildNodes[4].InnerText,
+          };
+          
+          return Aliments;
+
+        }).ToList();
 	}
 }
