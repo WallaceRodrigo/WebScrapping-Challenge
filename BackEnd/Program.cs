7f04057ad99d46e989bc7695f1d7e46073d57ef4
Wallace Rodrigo <wallacerodrigo2004@gmail.com>
2024-04-13 12:36:35 -0300

    initial configs

---
+++ BackEnd/Program.cs
@@ -0,0 +1,53 @@
+using System.Text;
+using System.Text.Json.Serialization;
+using Microsoft.Extensions.DependencyInjection;
+using System.Security.Claims;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddControllersWithViews()
+                .AddJsonOptions(x => x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+builder.Services.AddHttpClient();
+
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy", builder =>
+    {
+        builder
+            .AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+    });
+
+    options.AddPolicy(name: MyAllowSpecificOrigins, policy =>
+    {
+    });
+});
+
+var app = builder.Build();
+
+app.UseCors("CorsPolicy");
+
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
