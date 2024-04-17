1a5a632f24aa67462fcdd81e7bc3b39fcf1784e2
Wallace Rodrigo <wallacerodrigo2004@gmail.com>
2024-04-17 16:08:13 -0300

    little fix

--- FrontEnd/src/pages/Home.tsx
+++ FrontEnd/src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import axios from 'axios';
 import './Home.css'
 import { Aliment, SingleComponent } from '../types/aliments.types';
@@ -9,27 +9,68 @@ function Home() {
   const [alimentData, setAlimentData] = useState<SingleComponent[]>([]);
   const [isLoading, setIsLoading] = useState(true);
 
-  const fetchData = async () => {
+  //const fetchData = async () => {
+  //  try {
+  //    // const response = await axios.post('https://localhost:7070/addAliment', [{}]);
+  //    const response = await axios.get('https://localhost:7070/getAllAliments/1/1');
+//
+  //    setApiResponse(response.data);
+  //  } catch (error) {
+  //    console.log('Ocorreu um erro ao fazer a requisição', error);
+  //  }
+  //}
+//
+  //useEffect(() => { }, [alimentData])
+//
+  //useEffect(() => {
+    //    const didMount = async () => {
+    //      await fetchData()
+    //      setIsLoading(false);
+    //    }
+    //
+    //    didMount();
+  //}, [])
+
+  const onClickExtract = async () => {
+    const pageNumber = (document.querySelector('#pageInput') as HTMLInputElement).value;
+
+    if (pageNumber === '') {
+      alert('Digite o número da página que deseja extrair');
+      return;
+    }
+
+    if (Number(pageNumber) < 1 || Number(pageNumber) > 57) {
+      alert('Digite um número entre 1 e 57');
+      return;
+    }
+
     try {
-      // const response = await axios.post('https://localhost:7070/addAliment', [{}]);
-      const response = await axios.get('https://localhost:7070/');
+      const response = await axios.get(`https://localhost:7070/getAllAliments/${pageNumber}`);
+
+      //await axios.post(`https://localhost:7070/getAllAliments/${pageNumber}`)
 
       setApiResponse(response.data);
+      setIsLoading(false);
     } catch (error) {
       console.log('Ocorreu um erro ao fazer a requisição', error);
     }
   }
 
-  useEffect(() => { }, [alimentData])
+  const onClickSearch = async () => {
+    const searchInput = (document.querySelector('#searchInput') as HTMLInputElement).value;
 
-  useEffect(() => {
-    const didMount = async () => {
-      await fetchData()
-      setIsLoading(false);
+    if (searchInput === '') {
+      alert('Digite o nome do alimento que deseja pesquisar');
+      return;
     }
 
-    didMount();
-  }, [])
+    const filteredData = await axios.get(`https://localhost:7070/getAliment/${searchInput}`);
+
+    console.log(filteredData);
+    
+
+    setApiResponse(filteredData.data);
+  }
 
   const onCLick = (data: SingleComponent[]) => {
     const componentsTableDiv = document.getElementById('componentsTableDiv');
@@ -41,35 +82,48 @@ function Home() {
   };
 
   return (
-    <div id='homeDiv'>
-      <h1>Home</h1>
-      <input type="text" placeholder="Pesquisar Alimento"/>
+    <div className='homeDiv'>
+      <h1>WebScrapping TBCA</h1>
       {
-        isLoading ? <h3>Loading...</h3> :
-          <table id="alimentsTable">
-            <thead>
-              <tr>
-                <th>Id</th>
-                <th>Alimento</th>
-                <th>Nome científico</th>
-                <th>Grupo</th>
-                <th>Marca</th>
-              </tr>
-            </thead>
-            <tbody>
-              {
-                apiResponse.map((data: Aliment) => (
-                  <tr onClick={() => onCLick(data.components)}>
-                    <td>{data.alimentId}</td>
-                    <td>{data.name}</td>
-                    <td>{data.scientificName}</td>
-                    <td>{data.group}</td>
-                    <td>{data.brand}</td>
-                  </tr>
-                ))
-              }
-            </tbody>
-          </table>
+        isLoading ?
+          <div className='homeDiv'>
+            <p>Digite o numero da pagina que deseja extrair de TBCA.net.br</p>
+            <div>
+              <input id="pageInput" type="text" placeholder="De 1 a 57" />
+              <button onClick={onClickExtract}>Extrair</button>
+            </div>
+          </div>
+          :
+          <div className='homeDiv'>
+            <div>
+              <input id="searchInput" type="text" placeholder="Pesquisar Alimento" />
+              <button onClick={onClickSearch}>Pesquisar</button>
+            </div>
+            <table id="alimentsTable">
+              <thead>
+                <tr>
+                  <th>Id</th>
+                  <th>Alimento</th>
+                  <th>Nome científico</th>
+                  <th>Grupo</th>
+                  <th>Marca</th>
+                </tr>
+              </thead>
+              <tbody>
+                {
+                  apiResponse.map((data: Aliment) => (
+                    <tr onClick={() => onCLick(data.components)}>
+                      <td>{data.alimentId}</td>
+                      <td>{data.name}</td>
+                      <td>{data.scientificName}</td>
+                      <td>{data.group}</td>
+                      <td>{data.brand}</td>
+                    </tr>
+                  ))
+                }
+              </tbody>
+            </table>
+          </div>
       }
       {
         alimentData.length < 1 ? <></> :
