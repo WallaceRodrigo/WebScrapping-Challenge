e8f3e9403132edfd4d77c8f35e0c892405d8fffb
Wallace Rodrigo <wallacerodrigo2004@gmail.com>
2024-04-18 01:43:46 -0300

    project finished v2

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -1,14 +1,14 @@
 import './App.css'
 import Home from './pages/Home'
+import ComponentsPage from './pages/ComponentsPage'
 import { Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/:alimentId" element={<ComponentsPage />} />
+    </Routes>
   )
 }
 
